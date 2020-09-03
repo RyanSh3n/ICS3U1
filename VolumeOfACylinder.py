83d39a703696bbcdd71cb9295c50c9f2dba4855f
whipIash <38384369+whipIash@users.noreply.github.com>
2020-09-03 13:12:20 -0400

    Delete VolumeOfACylinder.py

--- VolumeOfACylinder.py
+++
@@ -1,5 +0,0 @@
-import math
-radius = int(input("Please print the radius of the cylinder"))
-height = int(input("Please print the height of the cylinder"))
-volume = round(math.pi*radius*radius*height, 1)
-print("The volume of the cylinder is ", volume)
