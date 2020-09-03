3323944442aafeb91d3db4eaf5cf4fff48b68a60
whipIash <38384369+whipIash@users.noreply.github.com>
2020-09-03 12:44:51 -0400

    Delete AreaOfAField.py

--- AreaOfAField.py
+++
@@ -1,6 +0,0 @@
-print("Please print the length of the field in feet")
-length = int(input())
-print("Please print the width of the field in feet")
-width = int(input())
-area = (length*width)/43560
-print("The area of the field is ", area)
