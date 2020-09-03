c08cd3912c5090f4fda5b9e7f3f90480b72ffae3
whipIash <38384369+whipIash@users.noreply.github.com>
2020-09-03 12:56:06 -0400

    Delete UnitsOfTime.py

--- UnitsOfTime.py
+++
@@ -1,6 +0,0 @@
-days = int(input("Please print the number of days "))
-hours = int(input("Please print the number of hours "))
-minutes = int(input("Please print the number of minutes "))
-seconds = int(input("Please print the number of seconds "))
-tot = days*24*60*60+hours*60*60+minutes*60+seconds
-print("The total number of seconds in this duration is", tot)
